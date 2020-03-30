ea89a20637879390f692bce3a652654864380b14
sprindjack <sprindjack@hotmail.com>
2020-03-30 16:39:24 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/rosywrt/luci-theme-rosy.git
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
 ./scripts/feeds update -a
