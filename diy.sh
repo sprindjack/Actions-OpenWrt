ec008437b2d720d8065d47ff54751f4d666ac4dc
sprindjack <sprindjack@hotmail.com>
2020-04-01 09:43:40 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,6 +7,3 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-git clone https://github.com/rufengsuixing/luci-app-adguardhome
-./scripts/feeds update -a
-./scripts/feeds install -a
