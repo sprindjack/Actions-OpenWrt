34f2e2b21f96285f78e99851ca61a88e808095d5
sprindjack <sprindjack@hotmail.com>
2020-04-02 21:15:29 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,3 +9,5 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i 's/0xf60000/0x1fb0000/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 git clone https://github.com/rufengsuixing/luci-app-adguardhome
+./scripts/feeds update -a
+./scripts/feeds install -a
