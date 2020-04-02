b927dd0b44e64333695da2936f52489da086fd55
sprindjack <sprindjack@hotmail.com>
2020-04-02 21:07:40 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,3 +8,4 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i 's/0xf60000/0x1fb0000/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+git clone https://github.com/rufengsuixing/luci-app-adguardhome
