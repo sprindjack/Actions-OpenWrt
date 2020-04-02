b06b7d3100c0bf64c2d1fee3a52f357efcc6a3d5
sprindjack <sprindjack@hotmail.com>
2020-04-02 23:06:57 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,7 +7,7 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-# Change Memory to 32M
+# Change Flash to 32M
 sed -i 's/0xf60000/0x1fb0000/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 # Add AdGuard Home luci app
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/
