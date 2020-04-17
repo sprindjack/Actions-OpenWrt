fdda2ed99d449765aaa03ec30b8fb4d2a67996b3
sprindjack <sprindjack@hotmail.com>
2020-04-17 12:26:46 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,7 +5,8 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-git clone https://github.com/fw876/helloworld package/lean/luci-app-ssr-plus/
+git clone https://github.com/fw876/helloworld temp123/helloworld/
+mv temp/helloworld/luci-app-ssr-plus package/lean/luci-app-ssr-plus
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 # Change Flash to 32M
