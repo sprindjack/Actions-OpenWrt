81869ec3f7c3c1874b0f6036685f714f066eccb0
sprindjack <sprindjack@hotmail.com>
2020-04-17 11:52:53 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,8 +5,7 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-rm -rf ./tmp
-sed -i 's/\"#src-git\"/\"src-git\"/g' ./feeds.conf.default
+git clone https://github.com/fw876/helloworld/tree/master/luci-app-ssr-plus package/lean/luci-app-ssr-plus/
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 # Change Flash to 32M
@@ -14,6 +13,5 @@ sed -i 's/0xf60000/0x1fb0000/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 # Add AdGuard Home luci app
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci-app-adguardhome/
 # Update feeds
-./scripts/feeds clean
 ./scripts/feeds update -a
 ./scripts/feeds install -a
