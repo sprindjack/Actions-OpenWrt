dbeab63b2de18cc9c6ad1df7e632b926a19f3d2c
sprindjack <sprindjack@hotmail.com>
2020-04-20 08:52:25 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -12,6 +12,8 @@ mv temp123/helloworld/luci-app-ssr-plus package/lean/luci-app-ssr-plus
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 # Change Flash to 32M
 sed -i 's/0xf60000/0x1fb0000/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+# Fix WiFi disabled after reboot
+sed -i '2a\ifconfig rax0 up\nifconfig ra0 up\nifconfig apclix0 up\nifconfig apcli0 up' package/base-files/files/etc/rc.local
 # Add AdGuard Home luci app
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci-app-adguardhome/
 # Update feeds
