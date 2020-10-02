794c09ad34a448570c90ed221095723e440afb52
sprindjack <sprindjack@hotmail.com>
2020-10-02 15:45:13 +0800

    Update and rename diy.sh to diy-part1.sh

---
+++ diy-part1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Add SSRP
+git clone https://github.com/fw876/helloworld package/lean/luci-app-ssr-plus
+
+# Add OpenClash
+git clone https://github.com/vernesong/OpenClash package/lean/luci-app-openclash
+
+# Add AdGuard Home luci app
+# git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci-app-adguardhome/
