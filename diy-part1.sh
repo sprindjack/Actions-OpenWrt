58f367e0c5aff0bb9c36b123041512511e203513
sprindjack <sprindjack@hotmail.com>
2020-10-02 16:03:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Add SSRP
-git clone https://github.com/fw876/helloworld temp11/
-mv temp11/luci-app-ssr-plus package/lean/luci-app-ssr-plus
+git clone https://github.com/fw876/helloworld tempsource1/
+mv tempsource1/luci-app-ssr-plus package/lean/luci-app-ssr-plus
 
 # Add OpenClash
-git clone https://github.com/vernesong/OpenClash temp11/
-mv temp11/luci-app-openclash package/lean/luci-app-openclash
+git clone https://github.com/vernesong/OpenClash tempsource2/
+mv tempsource2/luci-app-openclash package/lean/luci-app-openclash
 
 # Add AdGuard Home luci app
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome temp11/
