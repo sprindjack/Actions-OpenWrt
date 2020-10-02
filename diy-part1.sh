f47e586a43f8d6f452c219600897a79f10b27e28
sprindjack <sprindjack@hotmail.com>
2020-10-02 18:11:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 # Add SSRP
-git clone https://github.com/fw876/helloworld tempsource1/
-mv tempsource1/luci-app-ssr-plus package/lean/luci-app-ssr-plus
+#git clone https://github.com/fw876/helloworld tempsource1/
+#mv tempsource1/luci-app-ssr-plus package/lean/luci-app-ssr-plus
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # Add OpenClash
-git clone https://github.com/vernesong/OpenClash tempsource2/
-mv tempsource2/luci-app-openclash package/lean/luci-app-openclash
+sed -i '$a src-git vernesong https://github.com/vernesong/OpenClash' feeds.conf.default
+#git clone https://github.com/vernesong/OpenClash tempsource2/
+#mv tempsource2/luci-app-openclash package/lean/luci-app-openclash
 
 # Add AdGuard Home luci app
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome temp11/
